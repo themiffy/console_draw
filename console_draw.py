948954ac1430f6bde581554173a1ef7d4149c73c
Михаил Кузьмин <misha@Mihails-MacBook-Pro.local>
2022-09-06 17:14:28 +0300

    clear func and refactor

--- console_draw.py
+++ console_draw.py
@@ -8,24 +8,20 @@ from typing import Tuple, Union
 SCREEN_SIZE_X = 80
 SCREEN_SIZE_Y = 24
 
-
-def draw() -> None:
-    for i in range(80):
-        time.sleep(0.075)
-        sys.stdout.write("\r" + '.'*i)
-        sys.stdout.flush()
-
-        
-    print('\n\n'+ text2art('GAVNO'))
-
-
 class Display:
-    def __init__(self, x_size: int, y_size: int) -> None:
+
+    def __init__(self, x_size: int, y_size: int, symbol: str = '#', empty_space_symbol: str = ' ') -> None:
         '''Initialize fields and generate pixels list'''
+
+        if ord(symbol[0]) < 32: 
+            self.symbol = '#'
+        else: 
+            self.symbol: str = symbol[0] # Makes sure length is 1
 
         self.x_size: int = x_size
         self.y_size: int = y_size
-        self.__pixels = [[' ' for _ in range(self.x_size)] for _ in range(self.y_size)]
+        self.empty_space_symbol = empty_space_symbol
+        self.__pixels = [[self.empty_space_symbol for _ in range(self.x_size)] for _ in range(self.y_size)]
 
     def draw(self) -> None:
         '''Generate string out of the pixels list and draw it single time'''
@@ -35,8 +31,13 @@ class Display:
 
     def set_pixel(self, x: int, y: int) -> None:
         ''' Debug function setting pixel '''
-        #print(len(self.__pixels), len(self.__pixels[1]))
-        self.__pixels[y][x] = '#'
+
+        self.__pixels[y][x] = self.symbol
+            
+    def clear(self) -> None:
+        ''' Reinitialize with same parameters'''
+
+        self.__init__(self.x_size, self.y_size, symbol = self.symbol, empty_space_symbol = self.empty_space_symbol)
 
     def _make_frame_string(self) -> None:
         ''' Generate string out of pixels list '''
@@ -57,14 +58,32 @@ def pixel_input(message) -> Union[Tuple, None]:
         return None
 
 
+FRAME_COUNT = 0
+V1 = [(40, 12), (41, 12), (41, 13), (40, 13)]
+def circle(radius):
+    global FRAME_COUNT
+    global V1
+
+    FRAME_COUNT += 1 if FRAME_COUNT != 3 else -3
+    return V1[FRAME_COUNT]
 
 if __name__ == '__main__':
     display = Display(SCREEN_SIZE_X, SCREEN_SIZE_Y)
     display.draw()
 
+    x = 0
+
     while True:
-        input_value = pixel_input('Draw x y: ')
-        if input_value: 
-            x, y = input_value
-            display.set_pixel(x, y)
-            display.draw()
+        x, y = circle(1)
+        display.clear()
+        display.set_pixel(x, y)
+        display.draw()
+        time.sleep(0.1)
+
+
+    # while True:
+    #     input_value = pixel_input('Draw x y: ')
+    #     if input_value: 
+    #         x, y = input_value
+    #         display.set_pixel(x, y)
+    #         display.draw()
--- tests/test_draw.py
+++ tests/test_draw.py
@@ -8,6 +8,24 @@ def test_set_first_pixel():
     test_disp._make_frame_string() # only for test 
     assert test_disp.frame_string[0] == '#'
 
+def test_set_first_pixel_another_symbol():
+    test_disp = console_draw.Display(console_draw.SCREEN_SIZE_X, console_draw.SCREEN_SIZE_Y, symbol = '*')
+    test_disp.set_pixel(0, 0)
+    test_disp._make_frame_string() # only for test 
+    assert test_disp.frame_string[0] == '*'
+
+def test_set_first_pixel_incorrect_symbol():
+    test_disp = console_draw.Display(console_draw.SCREEN_SIZE_X, console_draw.SCREEN_SIZE_Y, symbol = 'string length must be 1')
+    test_disp.set_pixel(0, 0)
+    test_disp._make_frame_string() # only for test 
+    assert test_disp.frame_string[0] == 's'
+
+def test_set_first_pixel_special_sym():
+    test_disp = console_draw.Display(console_draw.SCREEN_SIZE_X, console_draw.SCREEN_SIZE_Y, symbol = '\n')
+    test_disp.set_pixel(0, 0)
+    test_disp._make_frame_string() # only for test 
+    assert test_disp.frame_string[0] == '#'
+
 def test_set_last_pixel():
     ''' Max pixel coord is 1 less thas size (pixel count starts with 0) '''
 
